2950343970b72f9b4e0f4c16abb31eae70ebfd93
Linh Phan <phanthuclinh@gmail.com>
2017-03-05 15:09:52 -0800

    Linh finally commits

---
+++ app/src/main/java/kitchnpal/servicerequest/JsonObjectRequest.java
@@ -0,0 +1,33 @@
+package kitchnpal.servicerequest;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.Volley;
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
+import com.android.volley.Response;
+import com.android.volley.Response.ErrorListener;
+import com.android.volley.Response.Listener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.UnsupportedEncodingException;
+
+import kitchnpal.kitchnpal.R;
+
+/**
+ * Created by linhphan on 17-03-05.
+ */
+public class JsonObjectRequest {
+
+    TextView mTxtDisplay;
+    ImageView mImageView;
+    mTxtDisplay = (TextView) findViewById(R.id.txtDisplay);
+    
+
+
+}
+
