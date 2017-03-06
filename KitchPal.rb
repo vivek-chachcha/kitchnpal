ec4197d888d4abf811203d45f210baefca340574
Vivek Chachcha <vivek@seekmi.com>
2017-03-06 13:33:05 -0800

    Added put api for user

--- KitchPal.rb
+++ KitchPal.rb
@@ -4,12 +4,16 @@ require_relative 'Users.rb'
 
 get '/users/:email' do
 	return_msg = {}
-	result = get_user(params['email'])
-	if (result == nil)
-		return_msg[:status] = 'invalid user id'
+	if (params.key?("email") && params.key?("password"))
+		result = get_user(params['email'], params['password'])
+		if (result == nil)
+			return_msg[:status] = 'invalid user id'
+		else
+			return_msg[:status] = 'success'
+			return_msg[:user] = result.to_s
+		end
 	else
-		return_msg[:status] = 'success'
-		return_msg[:user] = result.to_s
+		return_msg[:status] = 'required parameters - email, password - not provided'
 	end
 	return_msg.to_json
 end
@@ -18,10 +22,28 @@ post '/users' do
 	return_msg = {}
 	if (params.key?("name") && params.key?("email") && params.key?("password"))
 		result = create_user(params)
-                return_msg[:user] = result
+		if (result.class == Hash)
+	                return_msg[:user] = result
+		else
+			return_msg[:status] = result
+		end
 	else
 		return_msg[:status] = 'required parameters - name, email, password - not provided'
 	end
-	return_msg[:parameters] = params.to_s
+	return_msg.to_json
+end
+
+put '/users/:email' do
+	return_msg = {}
+	if (params.key?("email") && params.key?("password"))
+		result = update_user(params['email'], params['password'])
+		if (result.class == Hash)
+			return_msg[:user] = result
+		else
+			return_msg[:status] = result
+		end
+	else
+		return_msg[:status] = 'required parameters - email - not provided'
+	end
 	return_msg.to_json
 end
--- Users.rb
+++ Users.rb
@@ -1,15 +1,13 @@
 require_relative 'Client.rb'
 
-def get_user(email)
+def get_user(email, pwd)
 	user = nil
         # connect to the MySQL server
         client = create_client()
-	stmt = client.prepare("SELECT * FROM User WHERE email = ?")
-	results = stmt.execute(email)
+	stmt = client.prepare("SELECT * FROM User WHERE email = ? AND password = ?")
+	results = stmt.execute(email, pwd)
 	if (results.count == 1)
-		results.each do |row|
-			user = row
-		end
+		user = results.first
 	end
 	user
 end
@@ -17,6 +15,14 @@ end
 def create_user(params)
 	user = nil
 	client = create_client()
+
+	#check if email already exists
+	count = client.prepare("SELECT COUNT(id) FROM User WHERE email = ?")
+	count = count.execute(params['email'])
+	if (count.count != 0)
+		return "User already exists!"
+	end
+
 	stmt = client.prepare("INSERT INTO User (name, password, email, calPerDay, isActive, preferences,
 				lacto_vegetarian, ovo_vegetarian, vegan, vegetarian, pescetarian, accessToken) VALUES
 				(?, ?, ?, ?, 1, ?, ?, ?, ?, ?, ?, ?)")
@@ -29,9 +35,41 @@ def create_user(params)
 	pescetarian = (params['diet'].include? "pescetarian") ? 1 : 0
 	accessToken = (0...8).map { (65 + rand(26)).chr }.join
 	results = stmt.execute(params['name'], params['password'], params['email'], calPerDay, preferences, lactoVeg, ovoVeg, vegan, vegetarian, pescetarian, accessToken)
-	returns = {}
+	
+	#retrieve inserted user
 	id = client.query("SELECT last_insert_id()")
 	id = id.first['last_insert_id()']
 	results = client.query("SELECT * FROM User WHERE id = '#{id}'")
 	results.first
+end
+
+def update_user(params)
+	user = nil
+	client = create_client()
+
+	#check if email already exists
+        count = client.prepare("SELECT COUNT(id) FROM User WHERE email = ?")
+        count = count.execute(params['email'])
+        if (count.count == 0)
+                return "User does not exist!"
+        end
+
+	stmt = client.prepare("UPDATE User
+				SET name = ?, password = ?, calPerDay = ?, preferences = ?,
+                                lacto_vegetarian = ?, ovo_vegetarian = ?, vegan = ?, vegetarian = ?, pescetarian = ?
+                        	WHERE email = ?")
+        calPerDay = (params['calPerDay'] == nil) ? 0 : params['calPerDay']
+        preferences = (params['preferences'] == 'cheapest') ? 'cheapest' : 'lowcal'
+        lactoVeg = (params['diet'].include? "lacto") ? 1 : 0
+        ovoVeg = (params['diet'].include? "ovo") ? 1 : 0
+        vegan = (params['diet'].include? "vegan") ? 1 : 0
+        vegetarian = (params['diet'].include? "vegetarian") ? 1 : 0
+        pescetarian = (params['diet'].include? "pescetarian") ? 1 : 0
+        results = stmt.execute(params['name'], params['password'], calPerDay, preferences, lactoVeg, ovoVeg, vegan, vegetarian, pescetariani, params['email'])
+        
+	#retrieve updated user
+	id = client.query("SELECT last_insert_id()")
+        id = id.first['last_insert_id()']
+        results = client.query("SELECT * FROM User WHERE id = '#{id}'")
+        results.first
 end
