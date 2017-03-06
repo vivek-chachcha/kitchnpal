3243314832b0242774ec586d026030f7fca7c70a
Vivek EC2 <vchachcha@hotmail.com>
2017-03-06 01:02:09 +0000

    Post and get for user working now.

--- KitchPal.rb
+++ KitchPal.rb
@@ -1,13 +1,27 @@
 require 'sinatra'
-require 'Users'
+require 'json'
+require_relative 'Users.rb'
 
-get '/users/:uid' do
+get '/users/:email' do
 	return_msg = {}
-	result = get_user(params['uid'])
-	if (result == null)
+	result = get_user(params['email'])
+	if (result == nil)
 		return_msg[:status] = 'invalid user id'
 	else
 		return_msg[:status] = 'success'
-	return_msg[:user] = result.to_json
+		return_msg[:user] = result.to_s
+	end
+	return_msg.to_json
+end
+
+post '/users' do
+	return_msg = {}
+	if (params.key?("name") && params.key?("email") && params.key?("password"))
+		result = create_user(params)
+                return_msg[:user] = result
+	else
+		return_msg[:status] = 'required parameters - name, email, password - not provided'
+	end
+	return_msg[:parameters] = params.to_s
 	return_msg.to_json
 end
--- Users.rb
+++ Users.rb
@@ -1,22 +1,37 @@
-require 'dbi'
+require_relative 'Client.rb'
 
-def get_user(uid)
-    user = null
-    begin
+def get_user(email)
+	user = nil
         # connect to the MySQL server
-        dbh = DBI.connect("DBI:Mysql:TESTDB:localhost", 
-                               "testuser", "test123")
-        sth = dbh.prepare("SELECT * FROM User WHERE id = ?")
-        sth.execute(uid)
-        user = sth.fetch[0]
-        sth.finish
-    rescue DBI::DatabaseError => e
-        puts "An error occurred"
-        puts "Error code:    #{e.err}"
-        puts "Error message: #{e.errstr}"
-    ensure
-        # disconnect from server
-        dbh.disconnect if dbh
-    end
-    return user
+        client = create_client()
+	stmt = client.prepare("SELECT * FROM User WHERE email = ?")
+	results = stmt.execute(email)
+	if (results.count == 1)
+		results.each do |row|
+			user = row
+		end
+	end
+	user
+end
+
+def create_user(params)
+	user = nil
+	client = create_client()
+	stmt = client.prepare("INSERT INTO User (name, password, email, calPerDay, isActive, preferences,
+				lacto_vegetarian, ovo_vegetarian, vegan, vegetarian, pescetarian, accessToken) VALUES
+				(?, ?, ?, ?, 1, ?, ?, ?, ?, ?, ?, ?)")
+	calPerDay = (params['calPerDay'] == nil) ? 0 : params['calPerDay']
+	preferences = (params['preferences'] == 'cheapest') ? 'cheapest' : 'lowcal'
+	lactoVeg = (params['diet'].include? "lacto") ? 1 : 0
+	ovoVeg = (params['diet'].include? "ovo") ? 1 : 0
+	vegan = (params['diet'].include? "vegan") ? 1 : 0
+	vegetarian = (params['diet'].include? "vegetarian") ? 1 : 0
+	pescetarian = (params['diet'].include? "pescetarian") ? 1 : 0
+	accessToken = (0...8).map { (65 + rand(26)).chr }.join
+	results = stmt.execute(params['name'], params['password'], params['email'], calPerDay, preferences, lactoVeg, ovoVeg, vegan, vegetarian, pescetarian, accessToken)
+	returns = {}
+	id = client.query("SELECT last_insert_id()")
+	id = id.first['last_insert_id()']
+	results = client.query("SELECT * FROM User WHERE id = '#{id}'")
+	results.first
 end
