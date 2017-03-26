892d2d56bedcc83de8d9f178107dddf90b922a8b
Mandy Fung <frozenera@hotmail.com>
2017-03-26 15:54:47 -0700

    Updated unit test for Diet

--- app/src/test/java/kitchnpal/kitchnpal/DietTest.java
+++ app/src/test/java/kitchnpal/kitchnpal/DietTest.java
@@ -18,30 +18,35 @@ public class DietTest {
     public void testStringToPescetarian() throws Exception {
         Diet d = Diet.stringToDiet("Pescetarian");
         assertEquals(Diet.PESCETARIAN, d);
+        assertEquals("Pescetarian", Diet.PESCETARIAN.getName());
     }
 
     @Test
     public void testStringToLactoVegetarian() throws Exception {
         Diet d = Diet.stringToDiet("Lacto Vegetarian");
         assertEquals(Diet.LACTO_VEGETARIAN, d);
+        assertEquals("Lacto Vegetarian", Diet.LACTO_VEGETARIAN.getName());
     }
 
     @Test
     public void testStringToOvoVegetarian() throws Exception {
         Diet d = Diet.stringToDiet("Ovo Vegetarian");
         assertEquals(Diet.OVO_VEGETARIAN, d);
+        assertEquals("Ovo Vegetarian", Diet.OVO_VEGETARIAN.getName());
     }
 
     @Test
     public void testStringToVegan() throws Exception {
         Diet d = Diet.stringToDiet("Vegan");
         assertEquals(Diet.VEGAN, d);
+        assertEquals("Vegan", Diet.VEGAN.getName());
     }
 
     @Test
     public void testStringToVegetarian() throws Exception {
         Diet d = Diet.stringToDiet("Vegetarian");
         assertEquals(Diet.VEGETARIAN, d);
+        assertEquals("Vegetarian", Diet.VEGETARIAN.getName());
     }
 
     @Test
