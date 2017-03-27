e13306b943df6ad70560cc8a7cb246f8bcca7bdc
Vivek EC2 <vchachcha@hotmail.com>
2017-03-27 22:39:55 +0000

    Fixed initial tests with global email variable

--- KitchnPalTest.rb
+++ KitchnPalTest.rb
@@ -1,22 +1,22 @@
 ENV['RACK_ENV'] = 'test'
 
-require 'KitchnPal'
+require_relative 'KitchnPal'
 require 'test/unit'
 require 'rack/test'
 
 class KitchnPalTest < Test::Unit::TestCase
         include Rack::Test::Methods
-	email = ('a'..'z').to_a.shuffle[0,8].join
+	$email = ('a'..'z').to_a.shuffle[0,8].join
 
         def app
                 Sinatra::Application
         end
 
         def test_create_user
-		post '/users', params = {:name => "TestUser", :email => email, :password => "testing"}
+		post '/users', params = {:name => "TestUser", :email => $email, :password => "testing"}
 		assert last_response.ok?
 		assert last_response.body.include?("TestUser")
-		assert last_response.body.include?(email)
+		assert last_response.body.include?($email)
 	end
 
 	def test_create_user_no_params
@@ -30,7 +30,7 @@ class KitchnPalTest < Test::Unit::TestCase
 	end
 
 	def test_create_user_repeat_email
-		post '/users', params = {:name => "TestUser", :email => email, :password => "testing"}
+		post '/users', params = {:name => "TestUser", :email => $email, :password => "testing"}
 		assert last_response.body.include?("User already exists")
 	end
 
