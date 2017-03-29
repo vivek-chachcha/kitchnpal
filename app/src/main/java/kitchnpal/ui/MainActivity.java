03f76e81d12b06eaea3c595414326a467e026b49
Linh Phan <phanthuclinh@gmail.com>
2017-03-29 12:32:04 -0700

    fixed changes and added conversion method

--- app/src/main/java/kitchnpal/ui/MainActivity.java
+++ app/src/main/java/kitchnpal/ui/MainActivity.java
@@ -417,6 +417,7 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
 
         if (arr.length < 6) {
             playErrorMessage("Command not supported");
+            return;
         }
 
         addIngredientToFridge(arr[5], arr[2], arr[3]);
@@ -443,151 +444,117 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
     }
 
     private void addIngredientToFridge(String name, String quantity, String type) {
-        String wordAmount = convertNumbertoWords(quantity);
-        double amount = Double.parseDouble(wordAmount);
+        double wordAmount = convertWordToDouble(quantity);
         QuantityType qt = QuantityType.stringToType(type);
-        Ingredient ingredient = new Ingredient(name, amount, qt);
+        Ingredient ingredient = new Ingredient(name, wordAmount, qt);
         FridgeDatabaseHelper helper = new FridgeDatabaseHelper(getApplicationContext());
         helper.addIngredient(ingredient);
     }
 
-    private String convertNumbertoWords(String quantity) {
+    private Double convertWordToDouble(String quantity) {
         boolean isValidInput = true;
-        long result = 0;
-        long finalResult = 0;
-
+        int result = 0;
+        int finalResult = 0;
         List<String> allowedStrings = Arrays.asList("zero", "one", "two",
                 "three", "four", "five", "six", "seven", "eight", "nine",
                 "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
                 "sixteen", "seventeen", "eighteen", "nineteen", "twenty",
                 "thirty", "forty", "fifty", "sixty", "seventy", "eighty",
-                "ninety", "hundred", "thousands", "million", "billion","trillion");
-
+                "ninety", "hundred", "thousands", "million", "billion", "trillion");
         if (quantity != null && quantity.length() > 0) {
             quantity = quantity.replaceAll("-", " ");
             quantity = quantity.toLowerCase().replaceAll(" and", " ");
             String[] splittedParts = quantity.trim().split("\\s+");
-
-            for (String s: splittedParts) {
-                if(!allowedStrings.contains(s)) {
+            for (String s : splittedParts) {
+                if (!allowedStrings.contains(s)) {
                     isValidInput = false;
-                    System.out.println("Invalid word found: " +s );
+                    System.out.println("Invalid word found: " + s);
                     break;
                 }
-                if(isValidInput) {
-                    for(String str : splittedParts)
-                    {
-                        if(str.equalsIgnoreCase("zero")) {
+                if (isValidInput) {
+                    for (String str : splittedParts) {
+                        if (str.equalsIgnoreCase("zero")) {
                             result += 0;
-                        }
-                        else if(str.equalsIgnoreCase("one")) {
+                        } else if (str.equalsIgnoreCase("one")) {
                             result += 1;
-                        }
-                        else if(str.equalsIgnoreCase("two")) {
+                        } else if (str.equalsIgnoreCase("two")) {
                             result += 2;
-                        }
-                        else if(str.equalsIgnoreCase("three")) {
+                        } else if (str.equalsIgnoreCase("three")) {
                             result += 3;
-                        }
-                        else if(str.equalsIgnoreCase("four")) {
+                        } else if (str.equalsIgnoreCase("four")) {
                             result += 4;
-                        }
-                        else if(str.equalsIgnoreCase("five")) {
+                        } else if (str.equalsIgnoreCase("five")) {
                             result += 5;
-                        }
-                        else if(str.equalsIgnoreCase("six")) {
+                        } else if (str.equalsIgnoreCase("six")) {
                             result += 6;
-                        }
-                        else if(str.equalsIgnoreCase("seven")) {
+                        } else if (str.equalsIgnoreCase("seven")) {
                             result += 7;
-                        }
-                        else if(str.equalsIgnoreCase("eight")) {
+                        } else if (str.equalsIgnoreCase("eight")) {
                             result += 8;
-                        }
-                        else if(str.equalsIgnoreCase("nine")) {
+                        } else if (str.equalsIgnoreCase("nine")) {
                             result += 9;
-                        }
-                        else if(str.equalsIgnoreCase("ten")) {
+                        } else if (str.equalsIgnoreCase("ten")) {
                             result += 10;
-                        }
-                        else if(str.equalsIgnoreCase("eleven")) {
+                        } else if (str.equalsIgnoreCase("eleven")) {
                             result += 11;
-                        }
-                        else if(str.equalsIgnoreCase("twelve")) {
+                        } else if (str.equalsIgnoreCase("twelve")) {
                             result += 12;
-                        }
-                        else if(str.equalsIgnoreCase("thirteen")) {
+                        } else if (str.equalsIgnoreCase("thirteen")) {
                             result += 13;
-                        }
-                        else if(str.equalsIgnoreCase("fourteen")) {
+                        } else if (str.equalsIgnoreCase("fourteen")) {
                             result += 14;
-                        }
-                        else if(str.equalsIgnoreCase("fifteen")) {
+                        } else if (str.equalsIgnoreCase("fifteen")) {
                             result += 15;
-                        }
-                        else if(str.equalsIgnoreCase("sixteen")) {
+                        } else if (str.equalsIgnoreCase("sixteen")) {
                             result += 16;
-                        }
-                        else if(str.equalsIgnoreCase("seventeen")) {
+                        } else if (str.equalsIgnoreCase("seventeen")) {
                             result += 17;
-                        }
-                        else if(str.equalsIgnoreCase("eighteen")) {
+                        } else if (str.equalsIgnoreCase("eighteen")) {
                             result += 18;
-                        }
-                        else if(str.equalsIgnoreCase("nineteen")) {
+                        } else if (str.equalsIgnoreCase("nineteen")) {
                             result += 19;
-                        }
-                        else if(str.equalsIgnoreCase("twenty")) {
+                        } else if (str.equalsIgnoreCase("twenty")) {
                             result += 20;
-                        }
-                        else if(str.equalsIgnoreCase("thirty")) {
+                        } else if (str.equalsIgnoreCase("thirty")) {
                             result += 30;
-                        }
-                        else if(str.equalsIgnoreCase("forty")) {
+                        } else if (str.equalsIgnoreCase("forty")) {
                             result += 40;
-                        }
-                        else if(str.equalsIgnoreCase("fifty")) {
+                        } else if (str.equalsIgnoreCase("fifty")) {
                             result += 50;
-                        }
-                        else if(str.equalsIgnoreCase("sixty")) {
+                        } else if (str.equalsIgnoreCase("sixty")) {
                             result += 60;
-                        }
-                        else if(str.equalsIgnoreCase("seventy")) {
+                        } else if (str.equalsIgnoreCase("seventy")) {
                             result += 70;
-                        }
-                        else if(str.equalsIgnoreCase("eighty")) {
+                        } else if (str.equalsIgnoreCase("eighty")) {
                             result += 80;
-                        }
-                        else if(str.equalsIgnoreCase("ninety")) {
+                        } else if (str.equalsIgnoreCase("ninety")) {
                             result += 90;
-                        }
-                        else if(str.equalsIgnoreCase("hundred")) {
+                        } else if (str.equalsIgnoreCase("hundred")) {
                             result *= 100;
-                        }
-                        else if(str.equalsIgnoreCase("thousand")) {
+                        } else if (str.equalsIgnoreCase("thousand")) {
                             result *= 1000;
                             finalResult += result;
-                            result=0;
-                        }
-                        else if(str.equalsIgnoreCase("million")) {
+                            result = 0;
+                        } else if (str.equalsIgnoreCase("million")) {
                             result *= 1000000;
                             finalResult += result;
-                            result=0;
-                        }
-                        else if(str.equalsIgnoreCase("billion")) {
+                            result = 0;
+                        } else if (str.equalsIgnoreCase("billion")) {
                             result *= 1000000000;
                             finalResult += result;
-                            result=0;
-                        }
-                        else if(str.equalsIgnoreCase("trillion")) {
+                            result = 0;
+                        } else if (str.equalsIgnoreCase("trillion")) {
                             result *= 1000000000000L;
                             finalResult += result;
-                            result=0;
+                            result = 0;
                         }
                     }
+                    finalResult += result;
+                    result = 0;
                 }
             }
         }
+        return Double.valueOf(finalResult);
     }
 
     private static class SelectBtnListener implements View.OnClickListener {
