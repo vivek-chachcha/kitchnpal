78c022a45a747e42fc48964198f1c80ba42c2094
Mandy Fung <frozenera@hotmail.com>
2017-03-29 14:55:17 -0700

    Split up register and sign in buttons

--- app/src/main/java/kitchnpal/ui/LoginActivity.java
+++ app/src/main/java/kitchnpal/ui/LoginActivity.java
@@ -3,6 +3,9 @@ package kitchnpal.ui;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
@@ -19,8 +22,11 @@ import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.text.Html;
+import android.text.Spanned;
 import android.text.TextUtils;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
@@ -29,6 +35,8 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -90,6 +98,39 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             @Override
             public void onClick(View view) {
                 attemptLogin();
+            }
+        });
+        TextView registerButton = (TextView) findViewById(R.id.register_button);
+        Spanned spanned = Html.fromHtml(getString(R.string.register_help_text));
+        registerButton.setText(spanned);
+        final Activity activity = this;
+        registerButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+                LayoutInflater li = LayoutInflater.from(activity);
+                final View view1 = li.inflate(R.layout.register_popup, null);
+                builder.setTitle("Register")
+                        .setView(view1)
+                        .setPositiveButton("Register", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                User user = User.getInstance();
+                                AutoCompleteTextView email = (AutoCompleteTextView) view1.findViewById(R.id.email_register);
+                                EditText password = (EditText) view1.findViewById(R.id.password_register);
+                                user.setEmail(email.getText().toString().trim());
+                                user.setPassword(password.getText().toString().trim());
+                                dbHelper.addUser(user);
+                                dialog.dismiss();
+                                Intent i = new Intent(getApplicationContext(), UserPreferenceActivity.class);
+                                i.putExtra("isNewUser", true);
+                                startActivity(i);
+                            }})
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }});
+                AlertDialog dialog = builder.create();
+                dialog.show();
             }
         });
 
@@ -177,6 +218,15 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         } else if (!isEmailValid(email)) {
             mEmailView.setError(getString(R.string.error_invalid_email));
             focusView = mEmailView;
+            cancel = true;
+        } else if (!dbHelper.checkUser(email)) {
+            // Check if user exists on local db
+            mEmailView.setError(getString(R.string.error_invalid_email));
+            focusView = mEmailView;
+            cancel = true;
+        } else if (!dbHelper.checkUserPassword(email, password)) {
+            mPasswordView.setError(getString(R.string.error_incorrect_password));
+            focusView = mPasswordView;
             cancel = true;
         }
 
@@ -318,11 +368,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 return false;
             }
 
-            // Check if user exists on local db
-            if (dbHelper.checkUser(mEmail)) {
-                return dbHelper.checkUserPassword(mEmail, mPassword);
-            }
-
             return true;
         }
 
@@ -340,10 +385,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 for (Recipe r : dbHelper.getFavourites(mEmail)) {
                     user.addFavourite(r);
                 }
-                if (!dbHelper.checkUser(mEmail)) {;
-                    dbHelper.addUser(user);
-                    i = new Intent(getApplicationContext(), UserPreferenceActivity.class);
-                    i.putExtra("isNewUser", true);
+                if (!dbHelper.checkUser(mEmail)) {
+                    mPasswordView.setError(getString(R.string.error_incorrect_password));
+                    mPasswordView.requestFocus();
+                    return;
                 }
                 startActivity(i);
             } else {
